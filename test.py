78d744c78f646e502f4d6e6b9007bf7b1e73c329
aoout <1477732102@qq.com>
2021-02-23 20:08:11 +0800

    clear

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-follow_status=0
-page=1
-uid=9145020
-url = f'https://api.bilibili.com/x/space/bangumi/follow/list?type=1&follow_status={follow_status}&pn={page}&ps=15&vmid={uid}&ts=1611454168274'
-r=requests.get(url)
-print(r.text)
