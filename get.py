9ac08b69617335b4a82c20d20211f42993d136f7
aoout <1477732102@qq.com>
2021-02-24 10:34:36 +0800

    to-do completed

--- get.py
+++ get.py
@@ -1,10 +1,11 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit,QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit,QMessageBox,QTableWidgetItem
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5 import QtCore, uic
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from threading import Thread, Lock
 import requests
 import json
+import time
 
 
 with open('Style.css', encoding='utf-8') as f:
@@ -20,10 +21,12 @@ class MyWindow(QMainWindow):
         self.setWindowIcon(QIcon("icon.png"))
         self.label.setPixmap(QPixmap('2233.png'))
         self.label.setScaledContents(True)
+        self.tablewidgets  = [self.tableWidget, self.tableWidget_2,
+                            self.tableWidget_3, self.tableWidget_4]
         self.show()
+        
+       
         self.resizeEvent('初始化')
-        self.listwidgets = [self.listWidget, self.listWidget_2,
-                            self.listWidget_3, self.listWidget_4]
         self.button_get.clicked.connect(self.getAni)
         self.button_contrast.clicked.connect(self.contrastAni)
         self.action.triggered.connect(self.get)
@@ -38,14 +41,17 @@ class MyWindow(QMainWindow):
         self.lineedit_get.returnPressed.connect(self.getAni)
         self.lineedit_contrast_2.returnPressed.connect(self.contrastAni)
         self.status = 'get'
+        self.action_5.triggered.connect(self.accelerate)
+        self.action_8.triggered.connect(self.slowdown)
+        self.action_9.triggered.connect(self.why)
+        self.speed='acceletate'
 
     def get(self):
         if self.status == 'get':
             pass
         else:
             self.status = 'get'
-            for x in self.listwidgets:
-                x.clear()
+            self.cleartable()
             self.statusbar.clearMessage()
             self.stack.setCurrentIndex(0)
 
@@ -54,23 +60,52 @@ class MyWindow(QMainWindow):
             pass
         else:
             self.status = 'contrast'
-            for x in self.listwidgets:
-                x.clear()
+            self.cleartable()
             self.statusbar.clearMessage()
             self.stack.setCurrentIndex(1)
 
+    def accelerate(self):
+        if self.speed=='acceletate':
+            pass
+        else:
+            self.speed=='acceletate'
+            self.statusbar.showMessage('已成功切换为加速模式', 5000)
+    def slowdown(self):
+        if self.speed=='slowdown':
+            pass
+        else:
+            self.speed='slowdown'
+            self.statusbar.showMessage('已成功切换为减速模式', 5000)
+    def why(self):
+        QMessageBox.information(self,'解释','如果使用减速模式，能够保证追番列表中“全部”这一栏的顺序与b站页面完全一致。反之则不能。')
+
+
     def getAni(self):
+        self.starttime=time.time()
+        self.cleartable()
         uid = self.lineedit_get.text()
         self.get_thread.getuid(uid)
+        self.get_thread.getspeed(self.speed)
         self.get_thread.start()
         self.statusbar.showMessage('正在努力查询中...', 0)
 
     def geted(self, animes):
+        
         for i in range(4):
             for Ani in animes[i]:
-                self.listwidgets[i].addItem(Ani)
+                curRow=0
+                self.tablewidgets[i].insertRow(curRow)
+                item=QTableWidgetItem(Ani[0])
+                self.tablewidgets[i].setItem(curRow,0,item)
+                item=QTableWidgetItem(Ani[1])
+                self.tablewidgets[i].setItem(curRow,1,item)
+
+                
+        
         animes_num = len(animes[0])
-        self.statusbar.showMessage(f'一共查询到{animes_num}条追番记录！', 5000)
+        self.endtime = time.time()
+        dtime=self.endtime-self.starttime
+        self.statusbar.showMessage(f'一共查询到{animes_num}条追番记录！本次查询一共花费了{dtime:.4}秒', 5000)
 
     def ungeted(self):
         QMessageBox.critical(self,'错误','该用户隐私设置未公开！')
@@ -79,24 +114,44 @@ class MyWindow(QMainWindow):
         QMessageBox.critical(self,'错误',f'第{num}用户隐私设置未公开！')
         self.statusbar.clearMessage()
     def contrastAni(self):
+        self.starttime=time.time()
+        self.cleartable()
         uid_1 = self.lineedit_contrast_1.text()
         uid_2 = self.lineedit_contrast_2.text()
         self.contrast_thread.getuid(uid_1, uid_2)
+        self.contrast_thread.getspeed(self.speed)
         self.contrast_thread.start()
         self.statusbar.showMessage('正在努力查询对比中...', 0)
 
     def contrasted(self, animes_1, animes_2):
         for i in range(4):
             for Ani in set(animes_1[i]).intersection(set(animes_2[i])):
-                self.listwidgets[i].addItem(Ani)
+                curRow=0
+                self.tablewidgets[i].insertRow(curRow)
+                item=QTableWidgetItem(Ani[0])
+                self.tablewidgets[i].setItem(curRow,0,item)
+                item=QTableWidgetItem(Ani[1])
+                self.tablewidgets[i].setItem(curRow,1,item)
         animes_num_1 = len(animes_1[0])
         animes_num_2 = len(animes_2[0])
         animes_num_com = len(set(animes_1[0]).intersection(set(animes_2[0])))
+        self.endtime = time.time()
+        dtime=self.endtime-self.starttime
         self.statusbar.showMessage(
-            f'分别查询到{animes_num_1}和{animes_num_2}条追番记录！其中有{animes_num_com}条相同！', 5000)
+            f'分别查询到{animes_num_1}和{animes_num_2}条追番记录！其中有{animes_num_com}条相同！本次对比一共花费了{dtime:.4}秒', 5000)
 
     def resizeEvent(self, event):
         self.label.resize(self.widget.size())
+        width=self.tablewidgets[self.tabWidget.currentIndex()].width()
+        for table in self.tablewidgets:
+            table.setColumnWidth(0,int(width*0.65)-1); 
+            table.setColumnWidth(1,int(width*0.35)); 
+
+    def cleartable(self):
+        for table in self.tablewidgets:
+            curRow=table.currentRow()
+            for i in range(curRow+1):
+                table.removeRow(curRow)
 
 
 class getAni_thread(QThread):
@@ -108,9 +163,11 @@ class getAni_thread(QThread):
 
     def getuid(self, uid):
         self.uid = uid
+    def getspeed(self,speed):
+        self.speed=speed
 
     def run(self):
-        animes = getANIMES(self.uid)
+        animes = getANIMES(self.uid,self.speed)
         if animes==False:
             self.Invisible_get_signal.emit()
         else:
@@ -126,20 +183,22 @@ class contrastAni_thread(QThread):
     def getuid(self, uid_1, uid_2):
         self.uid_1 = uid_1
         self.uid_2 = uid_2
+    def getspeed(self,speed):
+        self.speed=speed
 
     def run(self):
-        animes_1 = getANIMES(self.uid_1)
+        animes_1 = getANIMES(self.uid_1,self.speed)
         
         if animes_1==False:
             self.Invisible_contrast_signal.emit(1)
-        animes_2 = getANIMES(self.uid_2)
+        animes_2 = getANIMES(self.uid_2,self.speed)
         if animes_2==False:
             self.Invisible_contrast_signal.emit(2)
         else:
             self.contrast_signal.emit(animes_1, animes_2)
 
 
-def getANIMES(uid):
+def getANIMES(uid,speed):
     ANIMES = [[] for _ in range(4)]
     ANILock = Lock()
 
@@ -162,7 +221,8 @@ def getANIMES(uid):
             data = js['data']
             animes_num = data['total']
             pages_max = animes_num//15+1
-            Animes = [item['title'] for item in data['list']]
+            # Animes = [item['title'] for item in data['list']]
+            Animes = [(item['title'],item['season_type_name']) for item in data['list']]
             ANILock.acquire()
             [ANIMES[follow_status].append(x) for x in Animes]
             ANILock.release()
@@ -170,11 +230,17 @@ def getANIMES(uid):
 
     if not visibilitycheck():
         return False
-
-    for i in range(4):
-        thread = Thread(target=threadfun, args=(i,))
-        thread.start()
-        thread.join()
+    if speed=='acceletate':
+        for i in range(1,4):
+            thread = Thread(target=threadfun, args=(i,))
+            thread.start()
+            thread.join()
+            ANIMES[0]=ANIMES[1]+ANIMES[2]+ANIMES[3]
+    else:
+        for i in range(4):
+            thread = Thread(target=threadfun, args=(i,))
+            thread.start()
+            thread.join()
     return ANIMES
 
 
